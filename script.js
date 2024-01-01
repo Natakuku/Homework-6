b57cfcca2ad57b673d437692d6f6e7220c026820
“Nataliia” <kukobnikova@gmail.com>
2024-01-01 18:34:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = +prompt('Your first number');
+let b = +prompt('Your second number');
+let c = +prompt('Your third number');
+let sum = a + b + c; 
+let average = sum / 3;
+alert('You average number is ' + average);
